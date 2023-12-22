91ca03e8690b1678803b7446c2cdce7d53584b88
SamratPaul2000 <samratpaul5001x@gmail.com>
2023-12-23 00:04:09 +0530

    first commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="flex justify-between items-center" >
+      <section>
+        <img className="w-[50vw] h-full flex justify-center items-center" src="/assets/LOGIN.jpg" alt="LOGO" />
+      </section>
+      <section className="text-white">
+        <h1>Foodify</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
